7b936f22b1019bed4907731fa027a622072755ce
Zeljko Damnjanovic <z.damnjanovic@emakina.rs>
2021-11-26 14:01:58 +0100

    test commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('ok');
+import "./styles/main.scss";
+import navigation from "./js/componentExample.js";
+console.log("ok");
+
+document.body.insertAdjacentElement("afterbegin", navigation());
